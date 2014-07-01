82d9d54c464cd27b22f27838e3bb12ac800591d0
Dersu <awayme@gmail.com>
2014-07-01 11:44:50 +0800

    hard code for private submit

--- djangovoice/settings.py
+++ djangovoice/settings.py
@@ -3,3 +3,8 @@ from django.conf import settings
 BRAND_VIEW = getattr(settings, 'VOICE_BRAND_VIEW', 'djangovoice_home')
 ALLOW_ANONYMOUS_USER_SUBMIT = getattr(
     settings, 'VOICE_ALLOW_ANONYMOUS_USER_SUBMIT', False)
+
+if getattr(settings, 'PRIVATE_TITLE', None) and getattr(settings, 'PRIVATE_DESCRIPTION', None) and getattr(settings, 'PRIVATE_TYPE', None):
+    PRIVATE_TITLE = getattr(settings, 'PRIVATE_TITLE', None)
+    PRIVATE_DESCRIPTION = getattr(settings, 'PRIVATE_DESCRIPTION', None)
+    PRIVATE_TYPE = getattr(settings, 'PRIVATE_TYPE', None)
